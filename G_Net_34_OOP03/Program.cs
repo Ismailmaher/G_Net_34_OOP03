5b9880bfb2308d9d1e59d51a83ac7f1f0ca4bffa
Isamilmaher <ismailmaher6977@gmail.com>
2026-03-25 04:59:14 +0200

    three child classes

--- G_Net_34_OOP03/Program.cs
+++ G_Net_34_OOP03/Program.cs
@@ -170,6 +170,41 @@ namespace G_Net_34_OOP03
 
             //}
             #endregion
+
+            #region three child classes
+
+            //public class StandardTicket : Ticket
+            //{
+            //    public string SeatNumber { get; set; }
+            //    public StandardTicket(string movie, decimal price, string seat) : base(movie, price) => SeatNumber = seat;
+
+            //    public override string ToString() => base.ToString() + $" | Seat: {SeatNumber}";
+
+
+            //}
+
+            //public class VIPTicket : Ticket
+            //{
+            //    public bool LoungeAccess { get; set; }
+            //    public decimal ServiceFee { get; } = 50;
+            //    public VIPTicket(string movie, decimal price, bool lounge) : base(movie, price) => LoungeAccess = lounge;
+            //    public override string ToString() => base.ToString() + $" | Lounge: {(LoungeAccess ? "Yes" : "No")} | Service Fee: {ServiceFee} EGP";
+            //}
+
+            //public class IMAXTicket : Ticket
+            //{
+            //    public bool Is3D { get; set; }
+            //    public IMAXTicket(string movie, decimal price, bool is3D) : base(movie, price)
+            //    {
+            //        Is3D = is3D;
+            //        if (Is3D) Price += 30;
+            //    }
+
+            //    public override string ToString() => base.ToString() + $" | IMAX 3D: {(Is3D ? "Yes" : "No")}";
+            //}
+
+
+            #endregion
         }
     }
 }
